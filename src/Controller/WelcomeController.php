0399698c0e7453059e94406231a3265a13b46309
Jan Zalesiński <janzalesinski@gmail.com>
2024-03-12 21:32:47 +0100

    default language = pl

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class WelcomeController extends AbstractController
 {
     #[Route('/', name: 'app_welcome')]
-    public function index(): Response
+    public function index(Request $request, SessionInterface $session): Response
     {
+
         return $this->render('welcome/index.html.twig', [
             'controller_name' => 'WelcomeController',
         ]);
